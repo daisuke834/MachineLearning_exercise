e8a6db2e555ff64381f7fcc92081787f0a7072e0
Daisuke Hashimoto <hassy5407@gmail.com>
2016-05-22 18:48:44 -0700

    Added regularization to CNN

--- MNIST/TensorFlow_CNN_LeNet5.py
+++ MNIST/TensorFlow_CNN_LeNet5.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
 
 	_num_dataset = len(_X)
 	_rand_index = np.random.permutation(_num_dataset)
-	_valid_start_index = int(_num_dataset*0.6)
-	_test_start_index = int(_num_dataset*0.8)
+	_valid_start_index = int(_num_dataset*0.8)
+	_test_start_index = int(_num_dataset*0.9)
 	#_valid_start_index = int(1024)
 	#_test_start_index = int(2048)
 	_X_train = _X[_rand_index[:_valid_start_index]]
@@ -53,11 +53,12 @@ if __name__ == '__main__':
 	_num_hidden = 64
 	_graph = tf.Graph()
 	with _graph.as_default():
+		_tf_lambda = tf.placeholder(tf.float32)
 		_tf_alpha = tf.placeholder(tf.float32)
 		_tf_keep_prob = tf.placeholder(tf.float32)
 		_tf_X = tf.placeholder(tf.float32, shape=(_batch_size, _image_size, _image_size, 1))
 		_tf_y = tf.placeholder(tf.float32, shape=(_batch_size, _num_labels))
-		
+
 		_tf_X_valid = tf.constant(_X_valid_norm, dtype=tf.float32)
 		_tf_X_test = tf.constant(_X_test_norm, dtype=tf.float32)
 
@@ -85,7 +86,8 @@ if __name__ == '__main__':
 
 		_tf_logits = model(_tf_X, _tf_keep_prob)
 		_tf_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(_tf_logits, _tf_y))
-		_tf_optimizer = tf.train.AdagradOptimizer(_tf_alpha).minimize(_tf_loss)
+		_tf_loss_reg = _tf_loss + 0.5 * _tf_lambda * (tf.nn.l2_loss(_weights1) + tf.nn.l2_loss(_weights2) + tf.nn.l2_loss(_weights3) + tf.nn.l2_loss(_weights4))
+		_tf_optimizer = tf.train.AdagradOptimizer(_tf_alpha).minimize(_tf_loss_reg)
 		_tf_prediction = tf.nn.softmax(_tf_logits)
 
 	def accuracy(_predictions, _labels):
@@ -96,27 +98,32 @@ if __name__ == '__main__':
 	print 'Start Learning'
 	_time_start = time.time()
 	_alpha_list = np.logspace(-2,1,7)
-	_scores = np.ndarray(len(_alpha_list), dtype=float)
+	_lambda_list = np.logspace(-5,-1,9)
+	_scores = np.ndarray( (len(_alpha_list), len(_lambda_list) ), dtype=float)
 	for _al_index, _alpha in enumerate(_alpha_list):
-		with tf.Session(graph=_graph) as _session:
-			_session.run(tf.initialize_all_variables())
-			for _step in range(_num_steps):
-				_offset = (_step * _batch_size) % (_num_train_dataset - _batch_size)
-				_batch_data		= _X_train_norm[_offset:(_offset + _batch_size), :]
-				_batch_labels	= _y_train[_offset:(_offset + _batch_size)]
-				_feed_dict = {_tf_X:_batch_data, _tf_y:_batch_labels, _tf_alpha:_alpha, _tf_keep_prob:0.5}
-				_, _l, _predictions = _session.run([_tf_optimizer, _tf_loss, _tf_prediction], feed_dict=_feed_dict)
-			_predict_valid = _session.run(predict(_tf_X_valid))
-			_accuracy_valid = accuracy(_predict_valid, _y_valid)
-			_scores[_al_index] = _accuracy_valid
-			print 'alpha='+str(_alpha)+',\tValidAccuracy='+str(_accuracy_valid)
+		for _lam_index, _lambda in enumerate(_lambda_list):
+			with tf.Session(graph=_graph) as _session:
+				_session.run(tf.initialize_all_variables())
+				for _step in range(_num_steps):
+					_offset = (_step * _batch_size) % (_num_train_dataset - _batch_size)
+					_batch_data		= _X_train_norm[_offset:(_offset + _batch_size), :]
+					_batch_labels	= _y_train[_offset:(_offset + _batch_size)]
+					_feed_dict = {_tf_X:_batch_data, _tf_y:_batch_labels, _tf_lambda:_lambda, _tf_alpha:_alpha, _tf_keep_prob:0.5}
+					_, _l, _predictions = _session.run([_tf_optimizer, _tf_loss_reg, _tf_prediction], feed_dict=_feed_dict)
+					_predict_valid = _session.run(predict(_tf_X_valid))
+					_accuracy_valid = accuracy(_predict_valid, _y_valid)
+					_scores[_al_index, _lam_index] = _accuracy_valid
+			print 'alpha='+str(_alpha)+',\tlambda='+str(_lambda)+',\tValidAccuracy='+str(_accuracy_valid)
 
 	_best_accuracy_valid = None
 	_best_alpha = None
+	_best_lambda = None
 	for _al_index, _alpha in enumerate(_alpha_list):
-		if _best_accuracy_valid is None or _scores[_al_index]>_best_accuracy_valid:
-			_best_accuracy_valid = _scores[_al_index]
-			_best_alpha = _alpha_list[_al_index]
+		for _lam_index, _lambda in enumerate(_lambda_list):
+			if _best_accuracy_valid is None or _scores[_al_index, _lam_index]>_best_accuracy_valid:
+				_best_accuracy_valid = _scores[_al_index, _lam_index]
+				_best_alpha = _alpha_list[_al_index]
+				_best_lambda = _lambda_list[_lam_index]
 
 	with tf.Session(graph=_graph) as _session:
 		_session.run(tf.initialize_all_variables())
@@ -124,8 +131,8 @@ if __name__ == '__main__':
 			_offset = (_step * _batch_size) % (_num_train_dataset - _batch_size)
 			_batch_data		= _X_train_norm[_offset:(_offset + _batch_size), :]
 			_batch_labels	= _y_train[_offset:(_offset + _batch_size)]
-			_feed_dict = {_tf_X:_batch_data, _tf_y:_batch_labels, _tf_alpha:_best_alpha, _tf_keep_prob:0.5}
-			_, _l, _predictions = _session.run([_tf_optimizer, _tf_loss, _tf_prediction], feed_dict=_feed_dict)
+			_feed_dict = {_tf_X:_batch_data, _tf_y:_batch_labels, _tf_lambda:_best_lambda, _tf_alpha:_best_alpha, _tf_keep_prob:0.5}
+			_, _l, _predictions = _session.run([_tf_optimizer, _tf_loss_reg, _tf_prediction], feed_dict=_feed_dict)
 		_predict_test = _session.run(predict(_tf_X_test))
 		_accuracy_test = accuracy(_predict_test, _y_test)
 	_time_end = time.time()
@@ -138,11 +145,23 @@ if __name__ == '__main__':
 	print 'Test Accuracy:', _accuracy_test
 	print 'Test Error Rate:', (1.0-_accuracy_test)
 
-	plt.plot(_alpha_list, _scores)
-	plt.title('Accuracy at Validation Set: CNN LeNet-5')
+	plt.subplot(1, 2, 1)
+	for _al_index, _alpha in enumerate(_alpha_list):
+		plt.plot(_lambda_list, _scores[_al_index, :], label='Alpha='+str(_alpha))
+	plt.title('Accuracy at Valid Set')
+	plt.xscale('log')
+	plt.xlabel('lambda')
+	plt.ylabel('Accuracy at Valid Set')
+	plt.legend(loc='lower right')
+
+	plt.subplot(1, 2, 2)
+	for _lam_index, _lambda in enumerate(_lambda_list):
+		plt.plot(_alpha_list, _scores[:, _lam_index], label='lambda='+str(_lambda))
+	plt.title('Accuracy at Valid Set')
 	plt.xscale('log')
 	plt.xlabel('alpha')
 	plt.ylabel('Accuracy at Valid Set')
+	plt.legend(loc='lower right')
 	plt.show()
 
 	_p = np.random.random_integers(0, len(_X_test), 25)
@@ -155,4 +174,3 @@ if __name__ == '__main__':
 		plt.imshow(_data, cmap=cm.gray_r, interpolation='nearest')
 		plt.title(str(int(_label))+'/'+str(int(_predicted_label)), color='red')
 	plt.show()
-
